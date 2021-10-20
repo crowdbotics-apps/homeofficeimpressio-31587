63d9aa9edec1e76ccd95eb485039dc8dd174c8cc
Crowdbotics <team@crowdbotics.com>
2021-10-20 05:57:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "homeofficeimpressio_31587",
+  url: "https://homeofficeimpressio_31587.botics.co",
+  api: "https://homeofficeimpressio_31587.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
